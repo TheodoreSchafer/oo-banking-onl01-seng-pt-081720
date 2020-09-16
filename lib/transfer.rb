8150b5f78fff13071492a8912c2165e5c4535191
Theodore Schafer <theodore.schafer@gmail.com>
2020-09-16 17:29:02 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
 
   def execute_transaction
-    if valid? && sender.balance > amount && self.status == "complete"
+    if valid? && sender.balance < amount && self.status == "complete"
     end
 
 
