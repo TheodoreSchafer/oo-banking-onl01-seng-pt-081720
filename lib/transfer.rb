2a632ac08d9aab151b4ad3aae9e83efc7d548d2e
Theodore Schafer <theodore.schafer@gmail.com>
2020-09-16 18:12:46 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
-    elsif @sender.balance < @amount
+    elsif @sender.valid? = false
       @status = "rejected"
       return "Transaction rejected. Please check your account balance"
     end
