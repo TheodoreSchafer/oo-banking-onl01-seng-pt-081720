af2fde3f092d5d2d2467fe30a8b29393538ee900
Theodore Schafer <theodore.schafer@gmail.com>
2020-09-16 17:22:30 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,5 +1,5 @@
 class Transfer
-  attr_accessor :sender, :reciever, :amount, :status
+  attr_accessor :sender, :receiver, :amount, :status
 
   def initialize(sender, receiver, amount, status="pending")
     @sender = sender
