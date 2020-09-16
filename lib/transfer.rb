87b73deb0cf75627078b29a255d490eacee2ac2e
Theodore Schafer <theodore.schafer@gmail.com>
2020-09-16 18:25:11 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -13,7 +13,7 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance > @amount 
+    if @sender.balance > @amount and @status != "complete"
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
